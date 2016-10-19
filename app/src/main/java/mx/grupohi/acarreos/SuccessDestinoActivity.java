d173b09aa27ed04e5a33c759ec1bd354eb0c7652
Pacotreflip <Sealab2021>
2016-10-19 12:18:04 -0500

    Detalles de viaje avance

--- app/src/main/java/mx/grupohi/acarreos/SuccessDestinoActivity.java
+++ app/src/main/java/mx/grupohi/acarreos/SuccessDestinoActivity.java
@@ -2,12 +2,58 @@ package mx.grupohi.acarreos;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class SuccessDestinoActivity extends AppCompatActivity {
+
+    private TextView textViewCamion,
+            textViewCubicacion,
+            textViewMaterial,
+            textViewOrigen,
+            textViewFechaHoraSalida,
+            textViewDestino,
+            textViewFechaHoraLlegada,
+            textViewRuta,
+            textViewObservaciones;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_success_destino);
+
+        textViewCamion = (TextView) findViewById(R.id.textViewCamion);
+        textViewCubicacion = (TextView) findViewById(R.id.textViewCubicacion);
+        textViewMaterial = (TextView) findViewById(R.id.textViewMaterial);
+        textViewOrigen = (TextView) findViewById(R.id.textViewCamion);
+        textViewFechaHoraSalida = (TextView) findViewById(R.id.textViewFechaHora);
+        textViewDestino = (TextView) findViewById(R.id.textViewDestino);
+        textViewFechaHoraLlegada = (TextView) findViewById(R.id.textViewFechaHoraLlegada);
+        textViewRuta = (TextView) findViewById(R.id.textViewRuta);
+        textViewObservaciones = (TextView) findViewById(R.id.textViewObservaciones);
+
+        Button btnImprimir = (Button) findViewById(R.id.buttonImprimir);
+        Button btnShowList = (Button) findViewById(R.id.buttonShowList);
+        Button btnSalir = (Button) findViewById(R.id.buttonSalir);
+
+
+    }
+
+    public void fillInfo() {
+        Integer idViaje = getIntent().getIntExtra("idViaje", 0);
+        Viaje viaje = new Viaje(getApplicationContext());
+        viaje = viaje.find(idViaje);
+
+        textViewCamion.setText(viaje.camion.economico);
+        textViewCubicacion.setText(viaje.camion.capacidad + " m3");
+        textViewMaterial.setText(viaje.material.descripcion);
+        textViewOrigen.setText(viaje.origen.descripcion);
+
+       /* textViewFechaHoraSalida,
+        textViewDestino,
+        textViewFechaHoraLlegada,
+        textViewRuta,
+        textViewObservaciones;
+        */
     }
 }
