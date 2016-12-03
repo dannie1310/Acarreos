e78cd9aad5d210953fe660e22359901ebb8d6f7b
Daniela <dbenitezc@ghi.mx>
2016-12-02 18:45:22 -0600

    menu imagenes

--- app/src/main/java/mx/grupohi/acarreos/ImagenesActivity.java
+++ app/src/main/java/mx/grupohi/acarreos/ImagenesActivity.java
@@ -1,60 +1,110 @@
 package mx.grupohi.acarreos;
 
-import android.annotation.TargetApi;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Build;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v4.util.Pair;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.Menu;
+import android.support.v7.app.AlertDialog;
 import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.ImageButton;
-import android.widget.Toolbar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
+public class ImagenesActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
-public class ImagenesActivity extends AppCompatActivity
-        implements AdapterView.OnItemClickListener {
+
     private String x;
     private GridView gridView;
     private AdaptadorImagenes adaptador;
     List<ImagenesViaje> lista;
     ImageButton button;
+    ProgressDialog progressDialogSync;
+    private Intent listaViajes;
+
+    Usuario usuario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_imagenes);
-
+        listaViajes =new Intent(this, ListaViajesActivity.class);
+        usuario = new Usuario(this);
+        usuario = usuario.getUsuario();
         x= getIntent().getStringExtra("idviaje_neto");
         ImagenesViaje m = new ImagenesViaje(this);
-         m.getImagen(Integer.parseInt(x));
+        m.getImagen(Integer.parseInt(x));
         int numImagenes = m.getCount(Integer.parseInt(x));
         button= (ImageButton) findViewById(R.id.imageButton);
         button.setEnabled(true);
-       if(numImagenes != 4){
-           button.setOnClickListener(new View.OnClickListener() {
-               @Override
-               public void onClick(View v) {
-                   Intent intent = new Intent(getApplicationContext(), CamaraActivity.class);
-                   intent.putExtra("idviaje_neto", x);
-                   startActivity(intent);
-               }
-           });
-       }else{
-           button.setVisibility(View.GONE);
-       }
+        if(numImagenes != 4){
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(getApplicationContext(), CamaraActivity.class);
+                    intent.putExtra("idviaje_neto", x);
+                    startActivity(intent);
+                }
+            });
+        }else{
+            button.setVisibility(View.GONE);
+        }
         gridView = (GridView) findViewById(R.id.grid);
         adaptador = new AdaptadorImagenes(getApplicationContext());
         gridView.setAdapter(adaptador);
-        gridView.setOnItemClickListener(this);
+        gridView.setOnItemClickListener((AdapterView.OnItemClickListener) this);
 
+        final DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        if(drawer != null)
+            drawer.post(new Runnable() {
+                @Override
+                public void run() {
+                    for (int i = 0; i < drawer.getChildCount(); i++) {
+                        View child = drawer.getChildAt(i);
+                        TextView tvp = (TextView) child.findViewById(R.id.textViewProyecto);
+                        TextView tvu = (TextView) child.findViewById(R.id.textViewUser);
+                        TextView tvv = (TextView) child.findViewById(R.id.textViewVersion);
+
+                        if (tvp != null) {
+                            tvp.setText(usuario.descripcionBaseDatos);
+                        }
+                        if (tvu != null) {
+                            tvu.setText(usuario.nombre);
+                        }
+                        if (tvv != null) {
+                            tvv.setText("Versión " + String.valueOf(BuildConfig.VERSION_NAME));
+                        }
+                    }
+                }
+            });
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
+
 
 
     @Override
@@ -69,7 +119,7 @@ public class ImagenesActivity extends AppCompatActivity
         }
     }
 
-    @Override
+
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
         ImagenesViaje item = (ImagenesViaje) parent.getItemAtPosition(position);
         Intent intent = new Intent(this, ImagenDetalle.class);
@@ -87,5 +137,76 @@ public class ImagenesActivity extends AppCompatActivity
             ActivityCompat.startActivity(this, intent, activityOptions.toBundle());
         } else
             startActivity(intent);
+    }
+
+
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_home) {
+            Intent intent = getIntent();
+            finish();
+            startActivity(intent);
+        } else if (id == R.id.nav_sync) {
+            new AlertDialog.Builder(ImagenesActivity.this)
+                    .setTitle("¡ADVERTENCIA!")
+                    .setMessage("Se borrarán los registros de viajes almacenados en este dispositivo. \n ¿Deséas continuar con la sincronización?")
+                    .setNegativeButton("NO", null)
+                    .setPositiveButton("SI", new DialogInterface.OnClickListener() {
+                        @Override public void onClick(DialogInterface dialog, int which) {
+                            if (Util.isNetworkStatusAvialable(getApplicationContext())) {
+                                if(!Viaje.isSync(getApplicationContext())) {
+                                     progressDialogSync = ProgressDialog.show(ImagenesActivity.this, "Sincronizando datos", "Por favor espere...", true);
+                                    new Sync(getApplicationContext(), progressDialogSync).execute((Void) null);
+                                } else {
+                                    Toast.makeText(getApplicationContext(), "No es necesaria la sincronización en este momento", Toast.LENGTH_LONG).show();
+                                }
+                            } else {
+                                Toast.makeText(getApplicationContext(), R.string.error_internet, Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    })
+                    .create()
+                    .show();
+
+        } else if (id == R.id.nav_list) {
+            startActivity(listaViajes);
+        } else if (id == R.id.nav_logout) {
+            if(!Viaje.isSync(getApplicationContext())){
+                new AlertDialog.Builder(ImagenesActivity.this)
+                        .setTitle("¡ADVERTENCIA!")
+                        .setMessage("Hay viajes aún sin sincronizar, se borrarán los registros de viajes almacenados en este dispositivo,  \n ¿Deséas sincronizar?")
+                        .setNegativeButton("NO", null)
+                        .setPositiveButton("SI", new DialogInterface.OnClickListener() {
+                            @Override public void onClick(DialogInterface dialog, int which) {
+                                if (Util.isNetworkStatusAvialable(getApplicationContext())) {
+                                    progressDialogSync = ProgressDialog.show(ImagenesActivity.this, "Sincronizando datos", "Por favor espere...", true);
+                                    new Sync(getApplicationContext(), progressDialogSync).execute((Void) null);
+
+                                    Intent login_activity = new Intent(getApplicationContext(), LoginActivity.class);
+                                    usuario.destroy();
+                                    startActivity(login_activity);
+                                } else {
+                                    Toast.makeText(getApplicationContext(), R.string.error_internet, Toast.LENGTH_LONG).show();
+                                }
+                            }
+                        })
+                        .create()
+                        .show();
+            }
+            else {
+                Intent login_activity = new Intent(getApplicationContext(), LoginActivity.class);
+                usuario.destroy();
+                startActivity(login_activity);
+            }
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
